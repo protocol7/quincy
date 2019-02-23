257950eadcbf92221ab291a6b84c52421b7b1aef
Niklas Gustavsson <niklas@protocol7.com>
2019-02-23 11:48:42 +0100

    Add version test

--- src/test/java/com/protocol7/nettyquic/protocol/VersionTest.java
+++ src/test/java/com/protocol7/nettyquic/protocol/VersionTest.java
@@ -3,6 +3,7 @@ package com.protocol7.nettyquic.protocol;
 import static org.junit.Assert.assertEquals;
 
 import com.protocol7.nettyquic.TestUtil;
+import com.protocol7.nettyquic.utils.Hex;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import org.junit.Test;
@@ -25,6 +26,21 @@ public class VersionTest {
 
     TestUtil.assertBuffer("51474fff", bb);
     TestUtil.assertBufferExhusted(bb);
+  }
+
+  @Test
+  public void read() {
+    assertEquals(Version.QUIC_GO, Version.read(b("51474fff")));
+    assertEquals(Version.DRAFT_17, Version.read(b("ff000011")));
+    assertEquals(Version.FINAL, Version.read(b("00000001")));
+    assertEquals(Version.VERSION_NEGOTIATION, Version.read(b("00000000")));
+    assertEquals(Version.UNKNOWN, Version.read(b("abcdabcd")));
+  }
+
+  private ByteBuf b(String d) {
+    ByteBuf bb = Unpooled.buffer();
+    bb.writeBytes(Hex.dehex(d));
+    return bb;
   }
 
   @Test
