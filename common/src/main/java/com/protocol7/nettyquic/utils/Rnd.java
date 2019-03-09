b4dd6fce4cf9e18fe09111bedfdf0f37f3ea6b46
Niklas Gustavsson <niklas@protocol7.com>
2019-03-09 23:41:18 +0100

    Remove unused methods

--- common/src/main/java/com/protocol7/nettyquic/utils/Rnd.java
+++ common/src/main/java/com/protocol7/nettyquic/utils/Rnd.java
@@ -1,6 +1,5 @@
 package com.protocol7.nettyquic.utils;
 
-import com.google.common.primitives.UnsignedLong;
 import java.math.BigInteger;
 import java.security.SecureRandom;
 
@@ -8,10 +7,6 @@ public class Rnd {
 
   private static final BigInteger TWO = BigInteger.valueOf(2);
   private static SecureRandom rnd = new SecureRandom();
-
-  public static long rndLong() {
-    return rnd.nextLong();
-  }
 
   public static void rndBytes(byte[] b) {
     rnd.nextBytes(b);
@@ -25,12 +20,6 @@ public class Rnd {
 
   public static int rndInt(int min, int max) {
     return rnd.nextInt(max - min) + min;
-  }
-
-  public static UnsignedLong rndUnsignedLong() {
-    byte[] b = new byte[8];
-    rnd.nextBytes(b);
-    return UnsignedLong.valueOf(new BigInteger(b).abs().multiply(TWO));
   }
 
   public static int rndInt() {
